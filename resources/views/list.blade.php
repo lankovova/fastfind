9c1f73bca5a49616229ae8e28ebe71f94815bc6f
lankovova <lanko.vova@gmail.com>
2017-04-16 15:40:49 +0300

    adaptive work

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,8 +6,14 @@
 
 <div id="main" class="flexbox">
 	<div class="filters-container">
+		<div class="toggle-filters-plate">
+			<i class="fa fa-chevron-down" aria-hidden="true"></i>
+			Open filters
+		</div>
 		<div class="filters">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis efficitur justo sit amet volutpat. Vivamus vel mauris luctus velit egestas fringilla sed eget justo. Suspendisse potenti. Suspendisse id velit vitae urna ornare feugiat id quis ante. Praesent tempor eleifend est non blandit. Pellentesque pulvinar iaculis mattis. Donec a odio nisi. Nulla euismod lacinia ligula. Donec eros lacus, ultricies eu porttitor vitae, finibus id quam. Aenean pharetra massa nec condimentum ullamcorper. Aenean faucibus aliquet mi, malesuada pharetra lectus rhoncus at. Curabitur tempor aliquet dui cursus sollicitudin. Pellentesque maximus maximus malesuada. Nunc tincidunt ullamcorper risus et venenatis. Morbi gravida tincidunt nisl vel condimentum. Donec nec dignissim nisi. Fusce vehicula lacinia elit vitae lacinia. In iaculis ante quis sem venenatis aliquam. Donec congue, ante id suscipit placerat, risus magna dictum leo, sed ullamcorper velit risus at lorem. Etiam faucibus consequat libero at tempus. Donec nec magna elit. Proin venenatis, nulla non efficitur venenatis, nunc lectus lacinia dolor, non congue sapien ex sed neque. Vestibulum augue augue, pulvinar eget odio sed, molestie semper nunc. Mauris egestas scelerisque nibh, sit amet facilisis massa efficitur non. Vestibulum luctus leo ullamcorper odio egestas, et sodales enim cursus. Sed consectetur purus vitae purus tincidunt faucibus. Aliquam ullamcorper dolor vitae augue luctus viverra.
+			<div class="filters-content">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis efficitur justo sit amet volutpat. Vivamus vel mauris luctus velit egestas fringilla sed eget justo. Suspendisse potenti. Suspendisse id velit vitae urna ornare feugiat id quis ante. Praesent tempor eleifend est non blandit. Pellentesque pulvinar iaculis mattis. Donec a odio nisi. Nulla euismod lacinia ligula. Donec eros lacus, ultricies eu porttitor vitae, finibus id quam. Aenean pharetra massa nec condimentum ullamcorper. Aenean faucibus aliquet mi, malesuada pharetra lectus rhoncus at. Curabitur tempor aliquet dui cursus sollicitudin. Pellentesque maximus maximus malesuada. Nunc tincidunt ullamcorper risus et venenatis. Morbi gravida tincidunt nisl vel condimentum. Donec nec dignissim nisi. Fusce vehicula lacinia elit vitae lacinia. In iaculis ante quis sem venenatis aliquam. Donec congue, ante id suscipit placerat, risus magna dictum leo, sed ullamcorper velit risus at lorem. Etiam faucibus consequat libero at tempus. Donec nec magna elit. Proin venenatis, nulla non efficitur venenatis, nunc lectus lacinia dolor, non congue sapien ex sed neque. Vestibulum augue augue, pulvinar eget odio sed, molestie semper nunc. Mauris egestas scelerisque nibh, sit amet facilisis massa efficitur non. Vestibulum luctus leo ullamcorper odio egestas, et sodales enim cursus. Sed consectetur purus vitae purus tincidunt faucibus. Aliquam ullamcorper dolor vitae augue luctus viverra.
+			</div>
 		</div>
 	</div>
 	<div class="cards-map-container">
@@ -17,7 +23,7 @@
 				<div class="card">
 					<div class="card-img">
 						<a href="#">
-							<img src="images/bar.jpg" alt="">
+							<img src="images/design.jpeg" alt="">
 						</a>
 					</div>
 					<div class="card-content">
@@ -39,7 +45,7 @@
 				<div class="card">
 					<div class="card-img">
 						<a href="#">
-							<img src="images/bar.jpg" alt="">
+							<img src="images/cool.jpg" alt="">
 						</a>
 					</div>
 					<div class="card-content">
