60aba36817648e688b59dc75f822e21f3eaedd7d
lankovova <lanko.vova@gmail.com>
2017-05-13 19:08:36 +0300

    changing template name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('homemy');
+        return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,62 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+@section('main')
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+	<div id="home-page">
+
+		<div class="image-container">
+			<img src="images/awesome.jpg" alt="">
+		</div>
+
+		<div class="main-block flexbox">
+			<div class="slogan-container">
+				<div class="slogan">
+					Easier than simple to find where to go
+				</div>
+			</div>
+			<div class="choose-type-container">
+				<div class="chose-type flexbox">
+					<div class="type-container">
+						<a href="{{ url('list') }}">
+							<div class="type"><i class="fa fa-cutlery"></i> Food</div>
+						</a>
+					</div>
+					<div class="type-container">
+						<a href="{{ url('list') }}">
+							<div class="type"><i class="fa fa-coffee"></i> Drink</div>
+						</a>
+					</div>
+					<div class="type-container">
+						<a href="{{ url('list') }}">
+							<div class="type"><i class="fa fa-ticket"></i> Money Waste</div>
+						</a>
+					</div>
+					<div class="type-container">
+						<a href="{{ url('list') }}">
+							<div class="type"><i class="fa fa-university"></i> Culture</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div id="footer">
+			<div class="autor-info flexbox">
+				<div class="autor">
+					Created by Lanko Vova &copy;
+				</div>
+				<div class="social-links">
+					<a href="https://github.com/lankovova" target="_blank">
+						<i class="fa fa-github fa-lg" aria-hidden="true"></i>
+					</a>
+					<a href="https://vk.com/boomshakalakas" target="_blank">
+						<i class="fa fa-vk fa-lg" aria-hidden="true"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+@stop
