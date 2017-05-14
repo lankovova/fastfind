b524d8cebb39b4f23d0d78260ed430e826949101
lankovova <lanko.vova@gmail.com>
2017-05-14 23:37:44 +0300

    footer fix + place design

--- resources/views/place.blade.php
+++ resources/views/place.blade.php
@@ -29,14 +29,50 @@
 
 	<section>
 		<div class="row">
-			Place details
+			<div class="place-info-container">
+				<div class="place-description">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</div>
+				<div class="place-details">
+					<div class="work-time">
+						8:00 - 23:00
+					</div>
+					<div class="address">
+						Kiev Khreschatuk street
+					</div>
+					<div class="telephone">
+						8-800-555-55-55
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
+
+	<section>
+		<div id="map"></div>
+	</section>
+
 	<section>
 		<div class="row">
-			<div id="map"></div>
+			Reviews
 		</div>
 	</section>
+</div>
+
+<div id="footer" class="nofix">
+	<div class="autor-info flexbox">
+		<div class="autor">
+			Created by Lanko Vova &copy;
+		</div>
+		<div class="social-links">
+			<a href="https://github.com/lankovova" target="_blank">
+				<i class="fa fa-github fa-lg" aria-hidden="true"></i>
+			</a>
+			<a href="https://vk.com/boomshakalakas" target="_blank">
+				<i class="fa fa-vk fa-lg" aria-hidden="true"></i>
+			</a>
+		</div>
+	</div>
 </div>
 
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_-fZhgOgEznL6N2NGE4VySEsYiErRHEQ"></script>
