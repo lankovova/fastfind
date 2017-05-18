44c2db846b398c006e4f16320455e7d0a9ee035a
lankovova <lanko.vova@gmail.com>
2017-05-18 22:37:05 +0300

    list page with all categories

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Place;
 
 class ListController extends Controller
 {
     public function index(Request $request)
     {
-        $chosenCategory = $request->input('c', 'all');
-
-        $category = Category::where('name', $chosenCategory)->first();
-        $places = $category->places->where('published', true);
+        if ($request->input('c')) {
+            $chosenCategory = $request->input('c', 'all');
+            $category = Category::where('name', $chosenCategory)->first();
+            $places = $category->places->where('published', true);
+        } else {
+            $places = Place::all()->where('published', true);
+        }
 
         return view('list', ['places' => $places]);
     }
