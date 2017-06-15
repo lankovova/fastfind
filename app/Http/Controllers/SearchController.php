437a7c6061741bdbd0efdef64ec6593992b062f8
lankovova <lanko.vova@gmail.com>
2017-06-15 19:31:48 +0300

    refactoring

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,20 +11,18 @@ class SearchController extends Controller
 	{
 		$searchText = $request->input('searchText');
 
-		// Get places that matches searchText
-		$places = Place::where('published', 1)
-					->where('name', 'like', '%'.$searchText.'%')
-					->orderBy('name', 'desc')
-					->take(10)
-					->get();
+		// Get places that matches searchText in any position
+		$places = Place::where("published", 1)
+						->where("name", "like", "%$searchText%")
+						->orderBy("name", "desc")
+						->take(10)
+						->get();
 
 		if (count($places) == 0) return;
 
-		$i = 0;
-		foreach ($places as $place) {
-			$matchablePlaces[$i]['id'] = $place['id'];
-			$matchablePlaces[$i]['name'] = $place['name'];
-			$i++;
+		for ($i = 0; $i < count($places); $i++) {
+			$matchablePlaces[$i]['id'] = $places[$i]['id'];
+			$matchablePlaces[$i]['name'] = $places[$i]['name'];
 		}
 
 		return json_encode($matchablePlaces);
