fd4589ce434271b77ce3e6da6bb7a3a027b63e4c
lankovova <lanko.vova@gmail.com>
2017-06-21 18:33:14 +0300

    pagination fix

--- public/js/app.js
+++ public/js/app.js
@@ -130,7 +130,9 @@ $(function(){
 
 });
 
-(function(){
+'use strict';
+
+(function () {
 
 	var pageNumber = 1;
 
@@ -139,88 +141,53 @@ $(function(){
 	var reqInProgress = false;
 
 	if ($('.cards-map-container').length !== 0) {
-		// Scroll event on places list page
-		$('.cards-map-container').scroll(function(e) {
+		$('.cards-map-container').scroll(function (e) {
 			if (therIsMorePlaces && !reqInProgress) {
 				if ($('#cards-end-marker').offset().top - $(window).height() === 0) {
 					reqInProgress = true;
-					$('#cards-preloader').css({display: 'flex'});
+					$('#cards-preloader').css({ display: 'flex' });
 
 					var filtersData = $('#filters-form').serialize();
 					var orderBy = $('#orderBy').val();
 					var orderFlow = $('#orderFlow').val();
 
-					$.get("/api/getPlaces?" + filtersData, { page: pageNumber, orderBy: orderBy, orderFlow: orderFlow })
-						.done(function(places) {
-							setTimeout(function() {
-								var i = 0;
-								places.forEach(function(place) {
-									// Insert retrived data to places template
-									var placeHTML = renderPlace(place);
+					$.get("/api/getPlaces?" + filtersData, { page: pageNumber, orderBy: orderBy, orderFlow: orderFlow }).done(function (places) {
+						setTimeout(function () {
+							var i = 0;
+							places.forEach(function (place) {
+								// Insert retrived data to places template
+								var placeHTML = renderPlace(place);
+								// Insert rendered places
+								$(placeHTML).appendTo($('.cards'));
+								i++;
+							});
 
-									// Remove preloader
-									$('#cards-preloader').css({display: 'none'});
+							if (i < 16) therIsMorePlaces = false;
 
-									// Insert rendered places
-									$(placeHTML).appendTo($('.cards'));
-
-									i++;
-								});
-
-								if (i < 16) therIsMorePlaces = false;
-
-								// Request is done
-								reqInProgress = false;
-								pageNumber++;
-							}, 1000);
-						});
+							// Remove preloader
+							$('#cards-preloader').css({ display: 'none' });
+							// Request is done
+							reqInProgress = false;
+							pageNumber++;
+						}, 1000);
+					});
 				}
 			}
 		});
 	}
 
 	function renderPlace(place) {
-		let priceHTML = '';
+		var priceHTML = '';
 
-		for (let i = 0; i < 5; i++) {
-			priceHTML += (i < place.average_price) ? "<i class='fa fa-usd filled' aria-hidden='true'></i>\n"
-													: "<i class='fa fa-usd default' aria-hidden='true'></i>\n";
+		for (var i = 0; i < 5; i++) {
+			priceHTML += i < place.average_price ? "<i class='fa fa-usd filled' aria-hidden='true'></i>\n" : "<i class='fa fa-usd default' aria-hidden='true'></i>\n";
 		}
 
-		let dots = '';
-		if (place.description > 100)
-			dots = '...';
+		var dots = '';
+		if (place.description > 100) dots = '...';
 
-		return `
-			<div class="card-container">
-				<div class="card">
-					<div class="card-img">
-						<a href="/place/${place.id}">
-							<img src="../images/places/${place.image}" alt="">
-						</a>
-					</div>
-					<div class="card-content">
-						<div class="card-title">
-							<a href="/place/${place.id}">${place.name}</a>
-						</div>
-						<div class="card-top-plate flexbox">
-							<div class="price">
-								${priceHTML}
-							</div>
-							<div class="rating">
-								${place.rating} <i class="fa fa-star" aria-hidden="true"></i>
-							</div>
-						</div>
-						<div class="card-description">
-							${place.description.substr(0,100)} ${dots}
-						</div>
-					</div>
-				</div>
-			</div>
-			`;
+		return '\n\t\t\t<div class="card-container">\n\t\t\t\t<div class="card">\n\t\t\t\t\t<div class="card-img">\n\t\t\t\t\t\t<a href="/place/' + place.id + '">\n\t\t\t\t\t\t\t<img src="../images/places/' + place.image + '" alt="">\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class="card-content">\n\t\t\t\t\t\t<div class="card-title">\n\t\t\t\t\t\t\t<a href="/place/' + place.id + '">' + place.name + '</a>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="card-top-plate flexbox">\n\t\t\t\t\t\t\t<div class="price">\n\t\t\t\t\t\t\t\t' + priceHTML + '\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="rating">\n\t\t\t\t\t\t\t\t' + place.rating + ' <i class="fa fa-star" aria-hidden="true"></i>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="card-description">\n\t\t\t\t\t\t\t' + place.description.substr(0, 100) + ' ' + dots + '\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t';
 	}
-
-
 })();
 
 $(document).ready(function(){
